577288decddfc7e2892397d90137e3665e81929e
luciencd <lucienchristiedervaux@gmail.com>
2025-03-10 17:10:31 -0400

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://luciencd.github.io/',
-	base: '',
+	base: '/',
 	integrations: [mdx(), sitemap()],
     outDir: './dist'
 });
